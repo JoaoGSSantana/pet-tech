68627e9c4944be6668f684f7d3708964527b3edb
João Gabriel de Sá Santana <joao.santana@amazonasinovare.com.br>
2025-02-12 16:52:39 -0300

    [feat] :sparkles: : Create create person route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 // Possui todas as configurações principais para a execução da API
 
 import fastify from 'fastify'
+import { personRoutes } from '@/http/controllers/person/routes'
 
 export const app = fastify()
+
+app.register(personRoutes)
