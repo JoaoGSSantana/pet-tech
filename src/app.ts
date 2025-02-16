5f74180d83e2d6d870d3f9889a95de38e48429df
João Gabriel Santana <joao.santana09@outlook.com>
2025-02-16 19:36:14 -0300

    [feat] :sparkles: : Add Resource not found error in global error handler

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { personRoutes } from '@/http/controllers/person/routes'
 import { userRoutes } from './http/controllers/user/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import { ResourceNotFoundError } from './use-cases/errors/resource-not-found'
 
 export const app = fastify()
 
@@ -16,6 +17,10 @@ app.setErrorHandler((error, _, reply) => {
     return reply
       .status(400)
       .send({ message: 'Validation Error', errors: error.errors })
+  }
+
+  if (error instanceof ResourceNotFoundError) {
+    return reply.status(404).send({ message: 'Resource Not Found' })
   }
 
   if (env.NODE_ENV === 'development') {
