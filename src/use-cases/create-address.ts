86458972649d80f94ab9a43ed56bb7619c772bda
João Gabriel Santana <joao.santana09@outlook.com>
2025-02-24 21:32:13 -0300

    [fix] :pencil: : Fix handler function in create address use case

--- src/use-cases/create-address.ts
+++ src/use-cases/create-address.ts
@@ -4,7 +4,7 @@ import { IAddressRepository } from '@/repositories/address.repository.interface'
 export class CreateAddressUseCase {
   constructor(private readonly addressRepository: IAddressRepository) {}
 
-  async handle(address: IAddress): Promise<IAddress | undefined> {
+  async handler(address: IAddress): Promise<IAddress | undefined> {
     return this.addressRepository.create(address)
   }
 }
