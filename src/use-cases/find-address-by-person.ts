b8d1d8bfece2678a13d75efccc75686ba62f4b58
João Gabriel Santana <joao.santana09@outlook.com>
2025-02-24 21:33:15 -0300

    [fix] :pencil: : Fix handle function name in find address by person usar case

--- src/use-cases/find-address-by-person.ts
+++ src/use-cases/find-address-by-person.ts
@@ -3,7 +3,7 @@ import { IAddressRepository } from '@/repositories/address.repository.interface'
 export class FindAddressByPersonUseCase {
   constructor(private addressRepository: IAddressRepository) {}
 
-  async handle(personId: number, page: number, limit: number) {
+  async handler(personId: number, page: number, limit: number) {
     return this.addressRepository.findAddressByPersonId(personId, page, limit)
   }
 }
