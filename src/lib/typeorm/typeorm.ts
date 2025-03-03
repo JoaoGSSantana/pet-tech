f445e00a0df100646d884c9ab0ce55d5a654e41d
João Gabriel Santana <joao.santana09@outlook.com>
2025-03-03 19:32:50 -0300

    [feat] :sparkles: : Create configuration file for typeorm

---
+++ src/lib/typeorm/typeorm.ts
@@ -0,0 +1,25 @@
+import { DataSource } from 'typeorm'
+
+import { Category } from '@/entities/category.entity'
+import { Product } from '@/entities/product.entity'
+import { env } from '@/env'
+
+export const appDataSource = new DataSource({
+  type: 'postgres',
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  username: env.DATABASE_USER,
+  password: env.DATABASE_PASSWORD,
+  database: env.DATABASE_NAME,
+  entities: [Product, Category],
+  logging: env.NODE_ENV === 'development',
+})
+
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log('Database typeorm connection established successfully')
+  })
+  .catch((error) => {
+    console.error('Error connecting to database typeorm', error)
+  })
