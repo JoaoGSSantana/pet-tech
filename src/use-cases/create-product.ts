eda32fcb424d7560387f32cac2d7cf84957b94ae
João Gabriel Santana <joao.santana09@outlook.com>
2025-03-03 22:17:02 -0300

    [feat] :sparkles: : Create create-product use case

---
+++ src/use-cases/create-product.ts
@@ -0,0 +1,9 @@
+import { IProduct } from '@/entities/models/product.interface'
+import { IProductRepository } from '@/repositories/product.repository.interface'
+
+export class CreateProductUseCase {
+  constructor(private readonly productRepository: IProductRepository) {}
+  async handler(product: IProduct): Promise<IProduct | undefined> {
+    return this.productRepository.create(product)
+  }
+}
