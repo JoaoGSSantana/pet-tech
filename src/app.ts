6b70e2319c691519442bfa251df9a0fdf1c877fa
João Gabriel Santana <joao.santana09@outlook.com>
2025-03-03 22:19:15 -0300

    [feat] :sparkles: : Register category and product routes

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ import '@/lib/typeorm/typeorm'
 import fastify from 'fastify'
 
 import { addressRoutes } from '@/http/controllers/address/routes'
+import { categoryRoutes } from '@/http/controllers/category/routes'
 import { personRoutes } from '@/http/controllers/person/routes'
+import { productRoutes } from '@/http/controllers/product/routes'
 import { userRoutes } from '@/http/controllers/user/routes'
 import { globalErrorHandler } from '@/utils/global-error-handler'
 
@@ -15,5 +17,7 @@ export const app = fastify()
 app.register(personRoutes)
 app.register(userRoutes)
 app.register(addressRoutes)
+app.register(productRoutes)
+app.register(categoryRoutes)
 
 app.setErrorHandler(globalErrorHandler)
