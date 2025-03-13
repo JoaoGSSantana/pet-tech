5e39238870b0aba0231c1480bb48637b20a04dd9
João Gabriel de Sá Santana <joao.santana@amazonasinovare.com.br>
2025-03-13 15:30:38 -0300

    [refactor] :recycle: : Rename resource not found error file

--- src/use-cases/find-product.ts
+++ src/use-cases/find-product.ts
@@ -1,6 +1,6 @@
 import { IProductRepository } from '@/repositories/product.repository.interface'
 
-import { ResourceNotFoundError } from './errors/resource-not-found'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 export class FindProductUseCase {
   constructor(private productRepository: IProductRepository) {}
--- src/use-cases/find-with-person.ts
+++ src/use-cases/find-with-person.ts
@@ -2,7 +2,7 @@ import { IPerson } from '@/entities/models/person.interface'
 import { IUser } from '@/entities/models/user.interface'
 import { IUserRepository } from '@/repositories/user.repository.interface'
 
-import { ResourceNotFoundError } from './errors/resource-not-found'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 export class FindWithPersonUseCase {
   constructor(private userRepository: IUserRepository) {}
